a14473e75a479de5f5dc6e6f8f471a7c17c8aca0
luise-vb <le.vonblumenthal@gmail.com>
2025-06-15 08:07:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const walks = [
     coords: [35.1532, 129.1185], // Gwangalli Beach
     title: "Gwangalli Beach",
     description: "Perfect during the week in low season but way too crowded on weekends or any day between June and September. Bella loves running in the sand and scaring the pigeons. 8/10",
-    image: "images/IMG_3621.jpeg
+    image: "images/IMG_3621.jpeg"
 "
   },
   {
